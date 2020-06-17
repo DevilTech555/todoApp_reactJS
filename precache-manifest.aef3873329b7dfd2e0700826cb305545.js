b4bfe87b4df90531e51286e7a828a4d684fa52d5
DevilTech555 <pmnaveen6@gmail.com>
2020-06-17 19:38:48 +0530

    Updates

---
+++ precache-manifest.aef3873329b7dfd2e0700826cb305545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c005936d785ce63df2534f24eed25ca",
+    "url": "/DevilTech555.github.io/todoApp_reactJS/index.html"
+  },
+  {
+    "revision": "ee129cf39a45ce4f924e",
+    "url": "/DevilTech555.github.io/todoApp_reactJS/static/css/main.c6862ec9.chunk.css"
+  },
+  {
+    "revision": "81f1bb1b70497b863ac4",
+    "url": "/DevilTech555.github.io/todoApp_reactJS/static/js/2.7a547fea.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/DevilTech555.github.io/todoApp_reactJS/static/js/2.7a547fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee129cf39a45ce4f924e",
+    "url": "/DevilTech555.github.io/todoApp_reactJS/static/js/main.f2247885.chunk.js"
+  },
+  {
+    "revision": "0ee354869f3780806b4d",
+    "url": "/DevilTech555.github.io/todoApp_reactJS/static/js/runtime-main.0c5d54fe.js"
+  }
+]);
